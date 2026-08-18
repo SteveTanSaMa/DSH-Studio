fb8100baa63eae63d050a3b624de41a965c0fe02
SteveTanSaMa <1519364926@qq.com>
2026-08-19 01:34:11 +0800

    Initial Commit

---
+++ DSH Studio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DSH Studio
+//
+//  Created by Steve Tan on 2026/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DSH Studio/DSH_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSH_StudioApp.swift
+//  DSH Studio
+//
+//  Created by Steve Tan on 2026/8/19.
+//
+
+import SwiftUI
+
+@main
+struct DSH_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
